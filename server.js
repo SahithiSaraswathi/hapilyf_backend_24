322bef762858778e14ee2d5d6450639074e9b7df
sahithisaraswathi <sahithisaraswathisadhu@gmail.com>
2023-04-24 22:54:55 +0530

    changes 1

--- server.js
+++ server.js
@@ -43,78 +43,55 @@ const options={
 }
 
 
-const swaggerSpec= swaggerJSDoc(options)
-app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
+// const swaggerSpec= swaggerJSDoc(options)
+// app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
 
 
 
 
-/**
- * @swagger
- *  components:
- *      schemas:
- *          User:
- *              type: object
- *              properties:
- *                  name:
- *                      type: string
- *                  email:
- *                      type: string
- *                  password:
- *                      type: string
- *          Doctor:
- *              type: object
- *              properties:
- *                  firstName:
- *                      type: string
- *                  lastName:
- *                      type: string
- *                  phoneNumber:
- *                      type: string 
- *                  website:
- *                      type:string
- *                  MedicalCertificate:
- *                      type:string
- *                  address:
- *                      type:string 
- *                  specialization:
- *                      type:string
- *                  experience:
- *                      type:integer
- *                  feePerConsultation:
- *                      type:integer
- *                  timings:
- *                      type:object
- *      
- */
- 
-/**
- * @swagger
- * /api/user/register:
- *  post:
- *      summary: create a new user
- *      description: create new user with given user details and adds to the database
- *      requestBody:
- *          required: true
- *          content:
- *              application/json:
- *                  schema:
- *                      $ref: '#components/schemas/User'
- *      responses:
- *          200:
- *              description: Success
- *              content:
- *                  aication/json:
- *                      schema:
- *                          type: array
- *                          items:
- *                              $ref: '#components/schemas/User'
- *
- */
 // /**
 //  * @swagger
-//  * /api/doctor/update-doctor-profile:
-//  *  put:
+//  *  components:
+//  *      schemas:
+//  *          User:
+//  *              type: object
+//  *              properties:
+//  *                  name:
+//  *                      type: string
+//  *                  email:
+//  *                      type: string
+//  *                  password:
+//  *                      type: string
+//  *          Doctor:
+//  *              type: object
+//  *              properties:
+//  *                  firstName:
+//  *                      type: string
+//  *                  lastName:
+//  *                      type: string
+//  *                  phoneNumber:
+//  *                      type: string 
+//  *                  website:
+//  *                      type:string
+//  *                  MedicalCertificate:
+//  *                      type:string
+//  *                  address:
+//  *                      type:string 
+//  *                  specialization:
+//  *                      type:string
+//  *                  experience:
+//  *                      type:integer
+//  *                  feePerConsultation:
+//  *                      type:integer
+//  *                  timings:
+//  *                      type:object
+//  *      
+//  */
+ 
+// /**
+//  * @swagger
+//  * /api/user/register:
+//  *  post:
 //  *      summary: create a new user
 //  *      description: create new user with given user details and adds to the database
 //  *      requestBody:
@@ -122,7 +99,7 @@ app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
 //  *          content:
 //  *              application/json:
 //  *                  schema:
-//  *                      $ref: '#components/schemas/Doctor'
+//  *                      $ref: '#components/schemas/User'
 //  *      responses:
 //  *          200:
 //  *              description: Success
@@ -131,34 +108,82 @@ app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
 //  *                      schema:
 //  *                          type: array
 //  *                          items:
-//  *                              $ref: '#components/schemas/Doctor'
+//  *                              $ref: '#components/schemas/User'
 //  *
 //  */
-// /**
-//  * @swagger
-//  * /api/user/register:
-//  *  post:
-//  *      summary: create a new user
-//  *      description: create new user with given user details and adds to the database
-//  *      consumes:
-//  *      - "multipart/form-data"
-//  *      produces:
-//  *      - "application/json"
-//  *      parameters:
-//  *      - name: "name"
-//  *        in: "formData"
-//  *        type: "string" 
-//  *      - name: "email"
-//  *        in: "formData"
-//  *        type: "string"
-//  *      - name: "password"
-//  *        in: "formData"
-//  *        type: "string"  
-//  *      responses:
-//  *          200:
-//  *              description: Success
-//  * 
-//  */
+// // /**
+// //  * @swagger
+// //  * /api/doctor/update-doctor-profile:
+// //  *  put:
+// //  *      summary: create a new user
+// //  *      description: create new user with given user details and adds to the database
+// //  *      requestBody:
+// //  *          required: true
+// //  *          content:
+// //  *              application/json:
+// //  *                  schema:
+// //  *                      $ref: '#components/schemas/Doctor'
+// //  *      responses:
+// //  *          200:
+// //  *              description: Success
+// //  *              content:
+// //  *                  aication/json:
+// //  *                      schema:
+// //  *                          type: array
+// //  *                          items:
+// //  *                              $ref: '#components/schemas/Doctor'
+// //  *
+// //  */
+// // /**
+// //  * @swagger
+// //  * /api/user/register:
+// //  *  post:
+// //  *      summary: create a new user
+// //  *      description: create new user with given user details and adds to the database
+// //  *      consumes:
+// //  *      - "multipart/form-data"
+// //  *      produces:
+// //  *      - "application/json"
+// //  *      parameters:
+// //  *      - name: "name"
+// //  *        in: "formData"
+// //  *        type: "string" 
+// //  *      - name: "email"
+// //  *        in: "formData"
+// //  *        type: "string"
+// //  *      - name: "password"
+// //  *        in: "formData"
+// //  *        type: "string"  
+// //  *      responses:
+// //  *          200:
+// //  *              description: Success
+// //  * 
+// //  */
+// // /**
+// //  * @swagger
+// //  * /api/doctor/update-doctor-profile:
+// //  *   put:
+// //  *     summary: Update a user's profile
+// //  *     consumes:
+// //  *       - "multipart/form-data"  
+// //  *     produces:
+// //  *       - "application/json"  
+// //  *     parameters:
+// //  *       - name: "firstName"
+// //  *         in: "formData"
+// //  *         schema:
+// //  *           type: string
+// //  *       - name: "lastName"
+// //  *         in: "formData"
+// //  *         type: string 
+// //   *     responses:
+// //  *       '200':
+// //  *         description: User profile updated successfully
+// //  *       '404':
+// //  *         description: User not found
+// //  *       '500':
+// //  *         description: Server error
+// //  */
 // /**
 //  * @swagger
 //  * /api/doctor/update-doctor-profile:
@@ -166,17 +191,41 @@ app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
 //  *     summary: Update a user's profile
 //  *     consumes:
 //  *       - "multipart/form-data"  
-//  *     produces:
-//  *       - "application/json"  
 //  *     parameters:
-//  *       - name: "firstName"
-//  *         in: "formData"
+//  *       - name: id
+//  *         in: path
+//  *         description: ID of the user to update
+//  *         required: true
 //  *         schema:
 //  *           type: string
-//  *       - name: "lastName"
-//  *         in: "formData"
-//  *         type: string 
-//   *     responses:
+//  *       - name: profile
+//  *         in: body
+//  *         description: Updated user profile
+//  *         required: true
+//  *         schema:
+//  *           type: object
+//  *           properties:
+//  *             firstName:
+//  *               type: string
+//  *             lastName:
+//  *               type: string
+//  *             phoneNumber:
+//  *               type: string 
+//  *             website:
+//  *               type: string
+//  *             MedicalCertificate:
+//  *               type: string
+//  *             address:
+//  *               type: string 
+//  *             specialization:
+//  *               type: string
+//  *             experience:
+//  *               type: integer
+//  *             feePerConsultation:
+//  *               type: integer
+//  *             timings:
+//  *               type: string
+//  *     responses:
 //  *       '200':
 //  *         description: User profile updated successfully
 //  *       '404':
@@ -184,138 +233,89 @@ app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerSpec))
 //  *       '500':
 //  *         description: Server error
 //  */
-/**
- * @swagger
- * /api/doctor/update-doctor-profile:
- *   put:
- *     summary: Update a user's profile
- *     consumes:
- *       - "multipart/form-data"  
- *     parameters:
- *       - name: id
- *         in: path
- *         description: ID of the user to update
- *         required: true
- *         schema:
- *           type: string
- *       - name: profile
- *         in: body
- *         description: Updated user profile
- *         required: true
- *         schema:
- *           type: object
- *           properties:
- *             firstName:
- *               type: string
- *             lastName:
- *               type: string
- *             phoneNumber:
- *               type: string 
- *             website:
- *               type: string
- *             MedicalCertificate:
- *               type: string
- *             address:
- *               type: string 
- *             specialization:
- *               type: string
- *             experience:
- *               type: integer
- *             feePerConsultation:
- *               type: integer
- *             timings:
- *               type: string
- *     responses:
- *       '200':
- *         description: User profile updated successfully
- *       '404':
- *         description: User not found
- *       '500':
- *         description: Server error
- */
-// app.put('/users/:id/profile', function (req, res) {
-//   // Handle PUT request to update user profile
-// });
+// // app.put('/users/:id/profile', function (req, res) {
+// //   // Handle PUT request to update user profile
+// // });
 
+
+// // /**
+// //  * @swagger
+// //  * /api/admin/get-all-doctors:
+// //  *  get:
+// //  *    summary: This api is used to check if get methods is working or not
+// //  *    description: This api is used to check if get methods is working or not
+// //  *    responses:
+// //  *        200:
+// //  *            description: To test get method
+// //  */
+
+
+
+// // /**
+// //  * @swagger
+// //  * /api/user/book-appointment:
+// //  *  post:
+// //  *    summary: This api is used to check if get methods is working or not
+// //  *    description: This api is used to post methods is working or not
+// //  *    responses:
+// //  *        200:
+// //  *            description: To get doctor info by id
+// //  */
 
 // /**
 //  * @swagger
-//  * /api/admin/get-all-doctors:
+//  * /api/admin/get-all-appointments:
 //  *  get:
 //  *    summary: This api is used to check if get methods is working or not
-//  *    description: This api is used to check if get methods is working or not
+//  *    description: This api is used to get all appointments
 //  *    responses:
 //  *        200:
-//  *            description: To test get method
+//  *            description: To get all appointments
 //  */
-
-
-
 // /**
 //  * @swagger
-//  * /api/user/book-appointment:
-//  *  post:
+//  * /api/admin/get-all-users:
+//  *  get:
 //  *    summary: This api is used to check if get methods is working or not
-//  *    description: This api is used to post methods is working or not
+//  *    description: This api is used to get all users
 //  *    responses:
 //  *        200:
-//  *            description: To get doctor info by id
+//  *            description: To get all users
 //  */
-
-/**
- * @swagger
- * /api/admin/get-all-appointments:
- *  get:
- *    summary: This api is used to check if get methods is working or not
- *    description: This api is used to get all appointments
- *    responses:
- *        200:
- *            description: To get all appointments
- */
-/**
- * @swagger
- * /api/admin/get-all-users:
- *  get:
- *    summary: This api is used to check if get methods is working or not
- *    description: This api is used to get all users
- *    responses:
- *        200:
- *            description: To get all users
- */
-/** 
-* @swagger
-* /api/admin/get-all-doctors:
-*  get:
-*    summary: This api is used to check if get methods is working or not
-*    description: This api is used to get all users
-*    responses:
-*        200:
-*            description: To get all users
-*/
-
-// /**
+// /** 
 // * @swagger
-// * /api/user/apply-doctor-account:
-// *  post:
-// *      summary: create a new doctor
-// *      description: create new doctor with given doctor details and adds to the database
-// *      requestBody:
-// *          required: true
-// *          content:
-// *           application/json:
-// *                  schema:
-// *                      $ref: '#components/schemas/Doctor'
-// *      responses:
-// *          200:
-// *              description: Success
-// *              content:
-// *                  application/json:
-// *                      schema:
-// *                          type: array
-// *                          items:
-// *                              $ref: '#components/schemas/Doctor'
-// *
+// * /api/admin/get-all-doctors:
+// *  get:
+// *    summary: This api is used to check if get methods is working or not
+// *    description: This api is used to get all users
+// *    responses:
+// *        200:
+// *            description: To get all users
 // */
+
+// // /**
+// // * @swagger
+// // * /api/user/apply-doctor-account:
+// // *  post:
+// // *      summary: create a new doctor
+// // *      description: create new doctor with given doctor details and adds to the database
+// // *      requestBody:
+// // *          required: true
+// // *          content:
+// // *           application/json:
+// // *                  schema:
+// // *                      $ref: '#components/schemas/Doctor'
+// // *      responses:
+// // *          200:
+// // *              description: Success
+// // *              content:
+// // *                  application/json:
+// // *                      schema:
+// // *                          type: array
+// // *                          items:
+// // *                              $ref: '#components/schemas/Doctor'
+// // *
+// // */
 app.get('/',(req,res)=>{
   res.send('Welcome to mongo db')
 })
